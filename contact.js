7a6cd044fa43b2f3fb578020a136a359d88b851d
Barbara Araujo <barbaraaraujo@MacBook-Air-de-Barbara.local>
2024-03-25 21:25:28 -0300

    feat: adding the MessageController class to handle HTTP requests and use NotionService to create pages in Notion

--- contact.js
+++ contact.js
@@ -4,17 +4,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const router = express.Router();
+class NotionService {
+  constructor() {
+    this.notion = new Client({
+      auth: process.env.NOTION_API_KEY,
+    });
+  }
 
-const notion = new Client({
-  auth: process.env.NOTION_API_KEY,
-});
-
-router.post('/', async (req, res) => {
-  const { name, email, message } = req.body;
-
-  async function create() {
-    const response = await notion.pages.create({
+  async createPage(name, email, message) {
+    const response = await this.notion.pages.create({
       parent: {
         type: 'database_id',
         database_id: 'c4d75e7e6cac43bd87aee6225e134e61',
@@ -50,12 +48,38 @@ router.post('/', async (req, res) => {
       },
     });
 
-    console.log(response);
+    return response;
+  }
+}
+
+class MessageController {
+  constructor() {
+    this.router = express.Router();
+    this.notionService = new NotionService();
+    this.initializeRoutes();
   }
 
-  await create();
+  initializeRoutes() {
+    this.router.post('/', this.postMessage.bind(this));
+  }
 
-  res.status(200).json({ message: 'Success' });
-});
+  async postMessage(req, res) {
+    try {
+      const { name, email, message } = req.body;
 
-export default router;
+      if (!name || !email || !message) {
+        return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
+      }
+
+      await this.notionService.createPage(name, email, message);
+
+      res.status(200).json({ message: 'Sucesso' });
+    } catch (error) {
+      console.error('Erro ao processar solicitação:', error);
+      res.status(500).json({ error: 'Ocorreu um erro ao processar a solicitação.' });
+    }
+  }
+}
+
+const messageController = new MessageController();
+export default messageController.router;
