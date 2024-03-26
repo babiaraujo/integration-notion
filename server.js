900de88b63f0af1a4f48d9938bd772560cf84128
Barbara Araujo <barbaraaraujo@MacBook-Air-de-Barbara.local>
2024-03-26 18:22:33 -0300

    build: refactor inicialize server

--- server.js
+++ server.js
@@ -7,15 +7,22 @@ import contactRouter from './contact.js';
 const app = express();
 
 app.use(cors());
-
 app.use(bodyParser.json());
 
 app.use('/api/contact', contactRouter);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+async function startServer() {
+  try {
+    await app.listen(PORT);
     console.log(`Server is running on port ${PORT}`);
-});
+  } catch (error) {
+    console.error('Error starting server:', error);
+    process.exit(1); 
+  }
+}
+
+startServer();
 
 export default app;
